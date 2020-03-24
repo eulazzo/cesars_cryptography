283428a991b1cdf10fa456f81633d14e2440f276
lazaro <lazaromello14@gmail.com>
2020-03-24 11:55:14 -0300

    first-commit/adding project

--- crypto.js
+++ crypto.js
@@ -3,8 +3,7 @@ const answerDecoded = require('./decoded');
 const text = answerDecoded;
 const alg = 'sha1';
 // const text = 'sometimes i think we are alone in the universe & sometimes i think we are not. in either case the idea is quite staggering arthur c. clarke';
-//cesarsCryptography
-//This project was made to apply for a Codenation's course (react)
+ 
 const crypted = crypto.createHash(alg)
         .update(text)
         .digest('hex');
